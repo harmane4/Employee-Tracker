8750677d67f74de3ced028411b65606982bcf4d7
Elise Harman <elise_harman@hotmail.com>
2021-05-23 17:43:15 +0800

    Inserted data into tables

--- employeeTracker.sql
+++ employeeTracker.sql
@@ -9,7 +9,7 @@ CREATE TABLE roles (
 id INT NOT NULL AUTO_INCREMENT,
  title NOT NULL VARCHAR(30), 
  salary NOT NULL DECIMAL(19,2)
- FOREIGN KEY (department_id) REFERENCES deoartment(id)
+ FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -19,3 +19,28 @@ id INT NOT NULL AUTO_INCREMENT,
 FOREIGN KEY (roles_id) REFERENCES roles(id)
   FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
+
+INSERT INTO department (departmentName) 
+VALUES ("Sales") , ("Engineering") , ("Finance") , ("Legal")
+
+INSERT INTO roles (title, salary, rolesID)
+VALUES ("Sales Lead", 10000, 1)
+
+INSERT INTO roles (title, salary, rolesID)
+VALUES ("Sales Person", 80000, 1)
+
+INSERT INTO roles (title, salary, rolesID)
+VALUES ("Lead Engineer", 150000, 2)
+
+INSERT INTO roles (title, salary, rolesID)
+VALUES ("Software Engineer", 120000, 2)
+
+INSERT INTO roles (title, salary, rolesID)
+VALUES ("Accountant", 125000, 3)
+
+INSERT INTO roles (title, salary, rolesID)
+VALUES ("Legal Team Lead", 250000, 4)
+
+INSERT INTO roles (title, salary, rolesID)
+VALUES ("Lawyer", 190000, 4)
+
